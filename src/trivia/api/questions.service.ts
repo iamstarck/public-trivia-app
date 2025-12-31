40031b9497aa10468f34f761f25a47726f71219e
Starck27 <adinataekwan27@gmail.com>
2025-12-31 11:56:45 +0700

    feat: refactor API services for categories and questions, implement hooks for data fetching

--- src/trivia/api/questions.service.ts
+++ src/trivia/api/questions.service.ts
@@ -1,46 +1,30 @@
 import { axiosInstance } from "@/lib/axios";
-import { clearToken, getToken } from "./token.service";
 
-export interface Question {
-  type: string;
-  difficulty: string;
-  category: string;
-  question: string;
-  correct_answer: string;
-  incorrect_answer: string[];
-}
+export const fetchQuestions = async (params: URLSearchParams) => {
+  const { data } = await axiosInstance.get(`/api.php?${params.toString()}`);
 
-interface QuestionResponse {
-  response_code: number;
-  results: Question[];
-}
-
-export interface QuestionParams {
-  amount: number;
-  type: "multiple" | "boolean";
-  category?: number;
-  difficulty?: "easy" | "medium" | "hard";
-}
-
-const fetchQuestions = async (params: QuestionParams): Promise<Question[]> => {
-  const token = await getToken();
-  const query = new URLSearchParams({ amount: String(params.amount), token });
-
-  if (params.category) query.append("category", String(params.category));
-  if (params.difficulty) query.append("difficulty", params.difficulty);
-  if (params.type) query.append("type", params.type);
-
-  const response = await axiosInstance.get<QuestionResponse>(
-    `/api.php?${query.toString()}`,
-  );
-
-  if (response.data.response_code === 3 || response.data.response_code === 4) {
-    clearToken();
-
-    return fetchQuestions(params);
-  }
-
-  return response.data.results;
+  return data;
 };
 
-export default fetchQuestions;
+// const fetchQuestions = async (params: QuestionParams): Promise<Question[]> => {
+//   const token = await getToken();
+//   const query = new URLSearchParams({ amount: String(params.amount), token });
+
+//   if (params.category) query.append("category", String(params.category));
+//   if (params.difficulty) query.append("difficulty", params.difficulty);
+//   if (params.type) query.append("type", params.type);
+
+//   const response = await axiosInstance.get<QuestionResponse>(
+//     `/api.php?${query.toString()}`,
+//   );
+
+//   if (response.data.response_code === 3 || response.data.response_code === 4) {
+//     clearToken();
+
+//     return fetchQuestions(params);
+//   }
+
+//   return response.data.results;
+// };
+
+// export default fetchQuestions;
