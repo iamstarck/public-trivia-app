5f2f3703a9fc0da39c02bbe4f3dc4299ca2803f2
Starck27 <adinataekwan27@gmail.com>
2026-02-06 18:43:00 +0700

    feat: refactor trivia section

--- src/views/sections/trivia/TriviaScreenSection.tsx
+++ src/views/sections/trivia/TriviaScreenSection.tsx
@@ -8,96 +8,28 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Spinner } from "@/components/ui/spinner";
-import { useQuestions } from "@/hooks/useQuestions";
-import { normalizeQuestions } from "@/lib/normalizer";
-import useTriviaStore from "@/stores/useTriviaStore";
-import AnswersButton from "@/views/components/AnswersButton";
 import ScoreBox from "@/views/components/ScoreBox";
 import { CircleCheckIcon, CircleXIcon } from "lucide-react";
-import { useEffect } from "react";
+import { useTriviaScreen } from "./useTriviaScreen";
+import AnswersButton from "@/views/components/AnswersButton";
 
 const TriviaScreenSection = () => {
   const {
     userName,
-    amount,
-    category,
     categoryName,
-    difficulty,
-    triviaType,
+    amount,
     questions,
-    currentIndex,
-    addAnswer,
     correct,
     incorrect,
+    isLoading,
+    error,
+    refetch,
+    currentQuestion,
+    currentIndex,
+    onHandleAnswer,
+    getButtonClass,
     answerFeedback,
-    answers,
-  } = useTriviaStore();
-
-  const { data, isLoading, error, refetch } = useQuestions({
-    amount,
-    type: triviaType,
-    category,
-    difficulty,
-  });
-
-  const setScreen = useTriviaStore((q) => q.setScreen);
-  const setQuestions = useTriviaStore((q) => q.setQuestions);
-  const submitAnswer = useTriviaStore((a) => a.submitAnswer);
-  const nextQuestion = useTriviaStore((q) => q.nextQuestion);
-  const showAnswerFeedback = useTriviaStore((s) => s.showAnswerFeedback);
-
-  useEffect(() => {
-    if (!error) return;
-
-    if (
-      typeof error === "object" &&
-      "type" in error &&
-      (error.type === "TOKEN_INVALID" || error.type === "TOKEN_EXHAUSTED")
-    ) {
-      setScreen("configuration");
-    }
-  }, [error, setScreen]);
-
-  useEffect(() => {
-    if (!data || !triviaType) return;
-
-    const normalized = normalizeQuestions(data, triviaType);
-    setQuestions(normalized);
-  }, [data, triviaType, setQuestions]);
-
-  const currentQuestion = questions[currentIndex];
-
-  const onHandleAnswer = (answerIndex: number) => {
-    if (answerFeedback) return;
-
-    const q = questions[currentIndex];
-    const isCorrect = answerIndex === q.correctIndex;
-
-    addAnswer(currentIndex, answerIndex);
-    submitAnswer(isCorrect);
-
-    showAnswerFeedback(true);
-    setTimeout(nextQuestion, 1500);
-  };
-
-  const getButtonClass = (answerIndex: number) => {
-    if (!answerFeedback) {
-      return "";
-    }
-
-    if (answerIndex === currentQuestion.correctIndex) {
-      return "bg-green-500";
-    }
-
-    if (
-      answerIndex === answers[currentIndex] &&
-      answerIndex !== currentQuestion.correctIndex
-    ) {
-      return "bg-primary";
-    }
-
-    return "";
-  };
+  } = useTriviaScreen();
 
   if (error) {
     return (
---
+++ src/views/sections/trivia/useTriviaScreen.ts
@@ -0,0 +1,106 @@
+import { useQuestions } from "@/hooks/useQuestions";
+import { normalizeQuestions } from "@/lib/normalizer";
+import useTriviaStore from "@/stores/useTriviaStore";
+import { useEffect } from "react";
+
+export const useTriviaScreen = () => {
+  const {
+    userName,
+    amount,
+    category,
+    categoryName,
+    difficulty,
+    triviaType,
+    questions,
+    currentIndex,
+    correct,
+    incorrect,
+    answerFeedback,
+    answers,
+    addAnswer,
+    submitAnswer,
+    nextQuestion,
+    showAnswerFeedback,
+    setQuestions,
+    setScreen,
+  } = useTriviaStore();
+
+  const { data, isLoading, error, refetch } = useQuestions({
+    amount,
+    type: triviaType,
+    category,
+    difficulty,
+  });
+
+  useEffect(() => {
+    if (!error) return;
+
+    if (
+      typeof error === "object" &&
+      "type" in error &&
+      (error.type === "TOKEN_INVALID" || error.type === "TOKEN_EXHAUSTED")
+    ) {
+      setScreen("configuration");
+    }
+  }, [error, setScreen]);
+
+  useEffect(() => {
+    if (!data || !triviaType) return;
+
+    const normalized = normalizeQuestions(data, triviaType);
+    setQuestions(normalized);
+  }, [data, triviaType, setQuestions]);
+
+  const currentQuestion = questions[currentIndex];
+
+  const onHandleAnswer = (answerIndex: number) => {
+    if (answerFeedback) return;
+
+    const q = questions[currentIndex];
+    if (!q) return;
+
+    const isCorrect = answerIndex === q.correctIndex;
+
+    addAnswer(currentIndex, answerIndex);
+    submitAnswer(isCorrect);
+
+    showAnswerFeedback(true);
+    setTimeout(nextQuestion, 1500);
+  };
+
+  const getButtonClass = (answerIndex: number) => {
+    if (!answerFeedback) {
+      return "";
+    }
+
+    if (answerIndex === currentQuestion.correctIndex) {
+      return "bg-green-500";
+    }
+
+    if (
+      answerIndex === answers[currentIndex] &&
+      answerIndex !== currentQuestion.correctIndex
+    ) {
+      return "bg-primary";
+    }
+
+    return "";
+  };
+
+  return {
+    userName,
+    categoryName,
+    amount,
+    questions,
+    correct,
+    incorrect,
+    isLoading,
+    error,
+    refetch,
+    currentQuestion,
+    currentIndex,
+    onHandleAnswer,
+    getButtonClass,
+    answerFeedback,
+  };
+};
